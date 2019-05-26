fab198b2a616a9db523ab143e2dce546077e292b
Michał Sokólski <michsok@gmail.com>
2019-05-26 20:25:14 +0200

    Formatting.

--- controller.py
+++ controller.py
@@ -1,120 +1,125 @@
-import datahandler 
+import datahandler
+
 
 class Controller:
 
-    def __init__(self, filename = None):
+    def __init__(self, filename=None):
         self.dataHandler = datahandler.DataHandler(filename)
-     
-    def calculate_mean(self, territory, year, mf=None):
-        print('Calculating mean pass rate for', territory,'from 2010 to', year)
-        modifier = self.get_modifier(mf)
-                
 
-        sql= ('SELECT AVG(x) '
-              'FROM (SELECT A.year, CAST(SUM(A.count) AS REAL)/SUM(B.count) AS x '
-              'FROM matura A, matura B ' 
-              'WHERE A.type="zdało" '
-              'AND B.type="przystąpiło" ' 
-              'AND A.territory=B.territory ' 
-              'AND A.year = B.year ' 
-              'AND A.gender = B.gender '+modifier+' AND A.territory=? '
-              'AND A.year <=? '
-              'GROUP BY A.year)')
-        result = self.dataHandler.raw_select(sql, (territory,year))
+    def calculate_mean(self, territory, year, mf=None):
+        print('Calculating mean pass rate for', territory, 'from 2010 to', year)
+        modifier = self.get_modifier(mf)
+
+        sql = ('SELECT AVG(x) '
+               'FROM (SELECT A.year, CAST(SUM(A.count) AS REAL)/SUM(B.count) AS x '
+               'FROM matura A, matura B '
+               'WHERE A.type="zdało" '
+               'AND B.type="przystąpiło" '
+               'AND A.territory=B.territory '
+               'AND A.year = B.year '
+               'AND A.gender = B.gender ' + modifier +
+               ' AND A.territory=? '
+               'AND A.year <=? '
+               'GROUP BY A.year)')
+
+        result = self.dataHandler.raw_select(sql, (territory, year))
 
         for row in result:
-            print('{:.2f}%'.format(row[0]*100))
+            print('{:.2f}%'.format(row[0] * 100))
 
     def calculate_yearly_pass_rate(self, territory, mf=None):
         print('Calculating yearly pass rate for', territory)
         modifier = self.get_modifier(mf)
-        
-        sql= ('SELECT A.year, CAST(SUM(A.count) AS REAL)/SUM(B.count) '
-              'FROM matura A, matura B ' 
-              'WHERE A.type="zdało" ' 
-              'AND B.type="przystąpiło" ' 
-              'AND A.territory=B.territory ' 
-              'AND A.year = B.year ' 
-              'AND A.gender = B.gender '+modifier+' AND A.territory=? '
-              'GROUP BY A.year')
+
+        sql = ('SELECT A.year, CAST(SUM(A.count) AS REAL)/SUM(B.count) '
+               'FROM matura A, matura B '
+               'WHERE A.type="zdało" '
+               'AND B.type="przystąpiło" '
+               'AND A.territory=B.territory '
+               'AND A.year = B.year '
+               'AND A.gender = B.gender ' + modifier +
+               ' AND A.territory=? '
+               'GROUP BY A.year')
         result = self.dataHandler.raw_select(sql, (territory,))
 
         for row in result:
-            print('{:d} {:.2f}%'.format(row[0], row[1]*100))  
+            print('{:d} {:.2f}%'.format(row[0], row[1] * 100))
 
     def find_best_territory(self, mf=None):
         print('Finding voivodeship with the best pass rate across the years')
         modifier = self.get_modifier(mf)
 
-        sql=('SELECT year, territory, MAX(x) '
-            'FROM '
-            '(SELECT A.year, A.territory, CAST(SUM(A.count) AS REAL)/SUM(B.count) AS x '
-                'FROM matura A, matura B '
-                'WHERE A.type="zdało" ' 
-                'AND B.type="przystąpiło" '
-                'AND A.territory = B.territory '
-                'AND A.year = B.year '
-                'AND A.gender = B.gender '+modifier+' '
-            'GROUP BY A.year, A.territory) X '
-            'GROUP BY year ')
+        sql = ('SELECT year, territory, MAX(x) '
+               'FROM '
+               '(SELECT A.year, A.territory, CAST(SUM(A.count) AS REAL)/SUM(B.count) AS x '
+                    'FROM matura A, matura B '
+                    'WHERE A.type="zdało" '
+                    'AND B.type="przystąpiło" '
+                    'AND A.territory = B.territory '
+                    'AND A.year = B.year '
+                    'AND A.gender = B.gender ' + modifier + ' '
+                    'GROUP BY A.year, A.territory) X '
+               'GROUP BY year ')
+
         result = self.dataHandler.raw_select(sql)
-        
+
         for row in result:
-            print('{:d}: {} {:.2f}%'.format(row[0], row[1], row[2]*100))  
-                                              
+            print('{:d}: {} {:.2f}%'.format(row[0], row[1], row[2] * 100))
+
     def find_regressive_territories(self, mf=None):
         print('Finding regressive voivodeships')
         modifier = self.get_modifier(mf)
 
-        sql=('SELECT X.year AS prev_year, Y.year AS  next_year , X.territory, X.x AS prev_rate, Y.x AS next_rate '
-            'FROM '
-            '(SELECT A.year, A.territory, CAST(SUM(A.count) AS REAL)/SUM(B.count) AS x '
-                'FROM matura A, matura B '
-                'WHERE A.type="zdało" '
-                'AND B.type="przystąpiło" '
-                'AND A.territory = B.territory '
-                'AND A.year = B.year '
-                'AND A.gender = B.gender '+modifier+' '
-                'GROUP BY A.year, A.territory) X, '
-            '(SELECT A.year, A.territory, CAST(SUM(A.count) AS REAL)/SUM(B.count) AS x '
-                'FROM matura A, matura B '
-                'WHERE A.type="zdało" '
-                'AND B.type="przystąpiło" '
-                'AND A.territory = B.territory '
-                'AND A.year = B.year '
-                'AND A.gender = B.gender '+modifier+' '
-                'GROUP BY A.year, A.territory) Y '
-            'WHERE X.year + 1 = Y.year '
-            'AND X.territory = Y.territory '
-            'AND X.x > Y.x')
+        sql = ('SELECT X.year AS prev_year, Y.year AS  next_year , X.territory, X.x AS prev_rate, Y.x AS next_rate '
+               'FROM '
+               '(SELECT A.year, A.territory, CAST(SUM(A.count) AS REAL)/SUM(B.count) AS x '
+                    'FROM matura A, matura B '
+                    'WHERE A.type="zdało" '
+                    'AND B.type="przystąpiło" '
+                    'AND A.territory = B.territory '
+                    'AND A.year = B.year '
+                    'AND A.gender = B.gender ' + modifier + ' '
+                    'GROUP BY A.year, A.territory) X, '
+                '(SELECT A.year, A.territory, CAST(SUM(A.count) AS REAL)/SUM(B.count) AS x '
+                    'FROM matura A, matura B '
+                    'WHERE A.type="zdało" '
+                    'AND B.type="przystąpiło" '
+                    'AND A.territory = B.territory '
+                    'AND A.year = B.year '
+                    'AND A.gender = B.gender ' + modifier + ' '
+                    'GROUP BY A.year, A.territory) Y '
+               'WHERE X.year + 1 = Y.year '
+               'AND X.territory = Y.territory '
+               'AND X.x > Y.x')
+
         result = self.dataHandler.raw_select(sql)
 
         for row in result:
-            print('{:19}  {:d}: {:.2f}% -> {:d}: {:.2f}%'.format(row[2], row[0], row[3]*100, row[1], row[4]*100))
+            print('{:19}  {:d}: {:.2f}% -> {:d}: {:.2f}%'.format(row[2], row[0], row[3] * 100, row[1], row[4] * 100))
 
     def compare_two_territories(self, territoryA, territoryB, mf=None):
         print('Comparing', territoryA, 'and', territoryB, 'across the years')
         modifier = self.get_modifier(mf)
-        
-        sql=('SELECT year, territory, MAX(X.x) ' 
-            'FROM '
-            '(SELECT A.year, A.territory, CAST(SUM(A.count) AS REAL)/SUM(B.count) as x '
-                'FROM matura A, matura B '
-                'WHERE A.type="zdało" '
-                'AND B.type="przystąpiło" '
-                'AND A.territory = B.territory '
-                'AND A.year = B.year '
-                'AND A.gender = B.gender '+modifier+' '
-                'AND (A.territory=? OR A.territory=?) ' 
-                'GROUP BY A.year, A.territory) X '
-            'GROUP BY year')
+
+        sql = ('SELECT year, territory, MAX(X.x) '
+               'FROM '
+               '(SELECT A.year, A.territory, CAST(SUM(A.count) AS REAL)/SUM(B.count) as x '
+               'FROM matura A, matura B '
+               'WHERE A.type="zdało" '
+               'AND B.type="przystąpiło" '
+               'AND A.territory = B.territory '
+               'AND A.year = B.year '
+               'AND A.gender = B.gender ' + modifier + ' '
+               'AND (A.territory=? OR A.territory=?) '
+               'GROUP BY A.year, A.territory) X '
+               'GROUP BY year')
         result = self.dataHandler.raw_select(sql, (territoryA, territoryB))
-        
+
         for row in result:
-            print('{} {:19} {:.2f}%'.format(row[0], row[1], row[2]*100))  
+            print('{} {:19} {:.2f}%'.format(row[0], row[1], row[2] * 100))
 
     def get_modifier(self, mf):
-        if(mf):
+        if (mf):
             if mf == 'mężczyźni':
                 print('For males only.')
             else:
@@ -122,4 +127,3 @@ class Controller:
             return 'AND A.gender= "' + mf + '"'
         else:
             return ''
-
--- matura.py
+++ matura.py
@@ -1,78 +1,103 @@
 import argparse, controller
+
 parser = argparse.ArgumentParser()
 
 voivodeships_list = ['Dolnośląskie',
-                'Kujawsko-pomorskie',
-                'Lubelskie',
-                'Lubuskie',
-                'Łódzkie',
-                'Małopolskie',
-                'Mazowieckie',
-                'Opolskie',
-                'Podkarpackie',
-                'Podlaskie',
-                'Pomorskie',
-                'Śląskie',
-                'Świętokrzyskie',
-                'Warmińsko-Mazurskie',
-                'Wielkopolskie',
-                'Zachodniopomorskie']
+                     'Kujawsko-pomorskie',
+                     'Lubelskie',
+                     'Lubuskie',
+                     'Łódzkie',
+                     'Małopolskie',
+                     'Mazowieckie',
+                     'Opolskie',
+                     'Podkarpackie',
+                     'Podlaskie',
+                     'Pomorskie',
+                     'Śląskie',
+                     'Świętokrzyskie',
+                     'Warmińsko-Mazurskie',
+                     'Wielkopolskie',
+                     'Zachodniopomorskie']
 
 years_list = range(2010, 2019)
 
 
 class UserInterface:
     def __init__(self):
-        self.parser = self.createParser()
-    
-    def createParser(self):
+        self.parser = self.create_parser()
+
+    def create_parser(self):
         gender_group = parser.add_mutually_exclusive_group()
 
-        gender_group.add_argument("-m", 
-                                  "--male", 
-                                  action="store_const", 
-                                  dest="gender", 
-                                  const="mężczyźni", 
-                                  help="Calculate statistics only for the males.", 
+        gender_group.add_argument("-m",
+                                  "--male",
+                                  action="store_const",
+                                  dest="gender",
+                                  const="mężczyźni",
+                                  help="Calculate statistics only for the males.",
                                   default=None)
 
-        gender_group.add_argument("-f", 
-                                  "--female", 
-                                  action="store_const", 
-                                  dest="gender", 
-                                  const="kobiety", 
-                                  help="Calculate statistics only for the females.", 
+        gender_group.add_argument("-f",
+                                  "--female",
+                                  action="store_const",
+                                  dest="gender",
+                                  const="kobiety",
+                                  help="Calculate statistics only for the females.",
                                   default=None)
 
-        parser.add_argument("--filename", 
-                            action="store", 
-                            type=str, 
-                            dest="filename", 
-                            help="Specify filename with data.", 
-                            required=False, 
+        parser.add_argument("--filename",
+                            action="store",
+                            type=str,
+                            dest="filename",
+                            help="Specify filename with data.",
+                            required=False,
                             default=None)
 
+        subparsers = parser.add_subparsers(dest='command',
+                                           help='Choose from {mean, yearly, best, regressive, compare}',
+                                           required=True,
+                                           metavar='COMMAND')
 
-        subparsers = parser.add_subparsers(dest='command', help='Choose from {mean, yearly, best, regressive, compare}', required=True, metavar='COMMAND')
+        mean_subparser = subparsers.add_parser('mean',
+                                               description="Calculate the mean matura pass rate for voivodeships across the years")
+        mean_subparser.add_argument('voivodeship',
+                                    type=str,
+                                    help='Voivodeship name',
+                                    choices=voivodeships_list,
+                                    metavar='VOIVODESHIP')
 
-        mean_subparser = subparsers.add_parser('mean', description="Calculate the mean matura pass rate for voivodeships across the years")
-        mean_subparser.add_argument('voivodeship', type=str,  help='Voivodeship name', choices=voivodeships_list, metavar='VOIVODESHIP')
-        mean_subparser.add_argument('year', type=int, help="Calculate data from 2010 to this year", choices=years_list, metavar='YEAR')
+        mean_subparser.add_argument('year',
+                                    type=int,
+                                    help="Calculate data from 2010 to this year",
+                                    choices=years_list,
+                                    metavar='YEAR')
+
         mean_subparser.set_defaults(func=self.calculate_mean)
 
-        yearly_subparser = subparsers.add_parser('yearly', description="Calculate the yearly pass rate for a target voivodeship ")
-        yearly_subparser.add_argument('voivodeship', type=str,  help='Voivodeship name.', choices=voivodeships_list, metavar='VOIVODESHIP')
+        yearly_subparser = subparsers.add_parser('yearly',
+                                                 description="Calculate the yearly pass rate for a target voivodeship ")
+        yearly_subparser.add_argument('voivodeship',
+                                      type=str,
+                                      help='Voivodeship name.',
+                                      choices=voivodeships_list,
+                                      metavar='VOIVODESHIP')
+
         yearly_subparser.set_defaults(func=self.calculate_yearly)
 
-        best_subparser = subparsers.add_parser('best', description="Find the voivodeship with the best pass rate across the years.")
+        best_subparser = subparsers.add_parser('best',
+                                               description="Find the voivodeship with the best pass rate across the years.")
         best_subparser.set_defaults(func=self.calculate_best)
 
-        regressive_subparser = subparsers.add_parser('regressive', description="Find voivodeships that have regressive pass rate.")
+        regressive_subparser = subparsers.add_parser('regressive',
+                                                     description="Find voivodeships that have regressive pass rate.")
         regressive_subparser.set_defaults(func=self.calculate_regressive)
 
-        compare_subparser = subparsers.add_parser('compare', description="Compare the yearly pass rates in the two voivodeships.")
-        compare_subparser.add_argument('voivodeship1', type=str,  help='First voivodeship name.', choices=voivodeships_list, metavar='VOIVODESHIP_1')
-        compare_subparser.add_argument('voivodeship2', type=str,  help='Scond voivodeship name.', choices=voivodeships_list, metavar='VOIVODESHIP_2')
+        compare_subparser = subparsers.add_parser('compare',
+                                                  description="Compare the yearly pass rates in the two voivodeships.")
+        compare_subparser.add_argument('voivodeship1', type=str, help='First voivodeship name.',
+                                       choices=voivodeships_list, metavar='VOIVODESHIP_1')
+        compare_subparser.add_argument('voivodeship2', type=str, help='Scond voivodeship name.',
+                                       choices=voivodeships_list, metavar='VOIVODESHIP_2')
         compare_subparser.set_defaults(func=self.calculate_compare)
         return parser
 
@@ -81,28 +106,25 @@ class UserInterface:
         self.c = controller.Controller(args.filename)
         args.func(args)
 
-
     def calculate_mean(self, args):
-    	self.c.calculate_mean(args.voivodeship, args.year, args.gender)
+        self.c.calculate_mean(args.voivodeship, args.year, args.gender)
 
     def calculate_yearly(self, args):
-    	self.c.calculate_yearly_pass_rate(args.voivodeship, args.gender)
-
+        self.c.calculate_yearly_pass_rate(args.voivodeship, args.gender)
 
     def calculate_best(self, args):
         self.c.find_best_territory(args.gender)
-   
 
     def calculate_regressive(self, args):
         self.c.find_regressive_territories(args.gender)
-       
 
     def calculate_compare(self, args):
         self.c.compare_two_territories(args.voivodeship1, args.voivodeship2, args.gender)
 
- 
+
 def main():
     ui = UserInterface()
     ui.parse()
+
 
 main()
