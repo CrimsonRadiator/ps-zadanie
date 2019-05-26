5e3c7faed03a2334cb5c5119dd3a50bb8f894e59
Michał Sokólski <michsok@gmail.com>
2019-05-26 18:54:41 +0200

    Add better file structure. Better display.

---
+++ datahandler.py
@@ -0,0 +1,52 @@
+from urllib.request import urlopen
+import csv
+import sqlite3
+
+class DataHandler:
+    def __init__(self, filename = None):
+        with sqlite3.connect('matura.db') as conn:
+            c = conn.cursor()
+
+            #check if table exists
+            c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='matura'")
+            exists = c.fetchone()
+            if not exists:
+         
+                if not filename:
+                    print("Downloading data from dane.gov.pl...")
+                    url = 'https://www.dane.gov.pl/media/resources/20190520/Liczba_os%C3%B3b_kt%C3%B3re_przystapi%C5%82y_lub_zda%C5%82y_egzamin_maturalny.csv'
+                   #url = 'https://www.dane.gov.pl/media/resources/20190513/Liczba_os%C3%B3b_kt%C3%B3re_przystapi%C5%82y_lub_zda%C5%82y_egzamin_maturalny.csv'
+                    with urlopen(url) as response:
+                        response = response.read().decode('cp1250')
+                else:
+                    print("Reading from file " + filename + "...")
+                    with open(filename) as f:
+                        response = f.read()
+           
+                c.execute('''CREATE TABLE matura(
+                                territory text,
+                                type text,
+                                gender text,
+                                year integer,
+                                count integer)''')
+
+                reader = csv.reader(response.split('\n'), delimiter=';')
+
+                #Skip header
+                next(reader, None)
+
+                for row in reader:
+                    if row and row[0] != "Polska":
+                        c.execute("INSERT INTO matura VALUES(?,?,?,?,?)", row)
+                conn.commit()
+
+    def raw_select(self, sql, arg = None):
+        with sqlite3.connect('matura.db') as conn:
+            c = conn.cursor()
+            if(arg):
+                c.execute(sql,arg)
+            else:
+                c.execute(sql)
+            return c.fetchall()
+
+
--- matura.py
+++ matura.py
@@ -1,184 +1,70 @@
-from urllib.request import urlopen
-import csv
-import sqlite3
+import argparse, controller
+parser = argparse.ArgumentParser()
 
 
-class DataHandler:
-    def __init__(self, filename = None):
-        with sqlite3.connect('matura.db') as conn:
-            c = conn.cursor()
+class UserInterface:
+    def __init__(self):
+        self.parser = self.createParser()
+    
+    def createParser(self):
+        gender_group = parser.add_mutually_exclusive_group()
+        gender_group.add_argument("-m", "--male", action="store_const", dest="gender", const="Mężczyźni", help="Calculate statistics only for the males.", default=None)
 
-            #check if table exists
-            c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='matura'")
-            exists = c.fetchone()
-            if not exists:
-         
-                if not filename:
-                    print("Downloading data from dane.gov.pl...")
-                    url = 'https://www.dane.gov.pl/media/resources/20190520/Liczba_os%C3%B3b_kt%C3%B3re_przystapi%C5%82y_lub_zda%C5%82y_egzamin_maturalny.csv'
-                   #url = 'https://www.dane.gov.pl/media/resources/20190513/Liczba_os%C3%B3b_kt%C3%B3re_przystapi%C5%82y_lub_zda%C5%82y_egzamin_maturalny.csv'
-                    with urlopen(url) as response:
-                        response = response.read().decode('cp1250')
-                else:
-                    print("Reading from file " + filename + "...")
-                    with open(filename) as f:
-                        response = f.read()
-           
-                c.execute('''CREATE TABLE matura(
-                                territory text,
-                                type text,
-                                gender text,
-                                year integer,
-                                count integer)''')
+        gender_group.add_argument("-f", "--female", action="store_const", dest="gender", const="Kobiety", help="Calculate statistics only for the females.", default=None)
 
-                reader = csv.reader(response.split('\n'), delimiter=';')
-
-                #Skip header
-                next(reader, None)
-
-                for row in reader:
-                    if row and row[0] != "Polska":
-                        c.execute("INSERT INTO matura VALUES(?,?,?,?,?)", row)
-                conn.commit()
-
-    def raw_select(self, sql, arg = None):
-        with sqlite3.connect('matura.db') as conn:
-            c = conn.cursor()
-            if(arg):
-                c.execute(sql,arg)
-            else:
-                c.execute(sql)
-            return c.fetchall()
+        parser.add_argument("--filename", action="store", type=str, dest="filename", help="Specify filename with data.", required=False, default=None)
 
 
-class Matura:
+        subparsers = parser.add_subparsers(dest='command', required=True)
 
-    def __init__(self, filename = None):
-        self.dataHandler = DataHandler(filename)
-     
-    def calculate_mean(self, territory, year, mf=None):
-        modifier = ''
-        if(mf):
-            modifier = 'AND A.gender = "' + mf +'" '
+        mean_subparser = subparsers.add_parser('mean', description="Calculate the mean matura pass rate for voivodeships across the years")
+        mean_subparser.add_argument('voivodeship', type=str,  help='Voivodeship name')
+        mean_subparser.add_argument('year', type=int)
+        mean_subparser.set_defaults(func=self.calculate_mean)
+
+        yearly_subparser = subparsers.add_parser('yearly', description="Calculate the yearly pass rate for a target voivodeship ")
+        yearly_subparser.add_argument('voivodeship', type=str,  help='Voivodeship name.')
+        yearly_subparser.set_defaults(func=self.calculate_yearly)
+
+        best_subparser = subparsers.add_parser('best', description="Find the voivodeship with the best pass rate across the years.")
+        best_subparser.set_defaults(func=self.calculate_best)
+
+        regressive_subparser = subparsers.add_parser('regressive', description="Find voivodeships that have regressive pass rate.")
+        regressive_subparser.set_defaults(func=self.calculate_regressive)
+
+        compare_subparser = subparsers.add_parser('compare', description="Compare the yearly pass rates in the two voivodeships.")
+        compare_subparser.add_argument('voivodeship1', type=str,  help='First voivodeship name.')
+        compare_subparser.add_argument('voivodeship2', type=str,  help='Scond voivodeship name.')
+        compare_subparser.set_defaults(func=self.calculate_compare)
+        return parser
+
+    def parse(self):
+        args = parser.parse_args()
+        self.c = controller.Controller(args.filename)
+        args.func(args)
+
+
+    def calculate_mean(self, args):
+    	self.c.calculate_mean(args.voivodeship, args.year, args.gender)
+
+    def calculate_yearly(self, args):
+    	self.c.calculate_yearly_pass_rate(args.voivodeship, args.gender)
+
+
+    def calculate_best(self, args):
+        self.c.find_best_territory(args.gender)
+   
+
+    def calculate_regressive(self, args):
+        self.c.find_regressive_territories(args.gender)
        
-        sql= ('SELECT AVG(x) '
-              'FROM (SELECT A.year, CAST(SUM(A.count) AS REAL)/SUM(B.count) AS x '
-              'FROM matura A, matura B ' 
-              'WHERE A.type="zdało" '
-              'AND B.type="przystąpiło" ' 
-              'AND A.territory=B.territory ' 
-              'AND A.year = B.year ' 
-              'AND A.gender = B.gender '+modifier+' AND A.territory=? '
-	      'AND A.year <=? '
-              'GROUP BY A.year)')
-        result = self.dataHandler.raw_select(sql, (territory,year))
-        print(territory)
-        for row in result:
-            print(row)
 
-    def calculate_yearly_pass_rate(self, territory, mf=None):
-        modifier = ''
-        if(mf):
-            modifier = 'AND A.gender = "' + mf +'" '
-        
-        sql= ('SELECT A.year, CAST(SUM(A.count) AS REAL)/SUM(B.count) '
-              'FROM matura A, matura B ' 
-              'WHERE A.type="zdało" ' 
-              'AND B.type="przystąpiło" ' 
-              'AND A.territory=B.territory ' 
-              'AND A.year = B.year ' 
-              'AND A.gender = B.gender '+modifier+' AND A.territory=? '
-              'GROUP BY A.year')
-        result = self.dataHandler.raw_select(sql, (territory,))
-        print(territory)
-        for row in result:
-            print(row)  
+    def calculate_compare(self, args):
+        self.c.compare_two_territories(args.voivodeship1, args.voivodeship2, args.gender)
 
-    def find_best_territory(self, mf=None):
-        modifier = ''
-        if(mf):
-            modifier = 'AND A.gender= "' + mf + '"'
-
-        sql=('SELECT year, territory, MAX(x) '
-            'FROM '
-            '(SELECT A.year, A.territory, CAST(SUM(A.count) AS REAL)/SUM(B.count) AS x '
-                'FROM matura A, matura B '
-                'WHERE A.type="zdało" ' 
-                'AND B.type="przystąpiło" '
-                'AND A.territory = B.territory '
-                'AND A.year = B.year '
-                'AND A.gender = B.gender '+modifier+' '
-            'GROUP BY A.year, A.territory) X '
-            'GROUP BY year ')
-        result = self.dataHandler.raw_select(sql)
-        
-        for row in result:
-            print(row)  
-                                              
-    def find_regressive_territories(self, mf=None):
-        modifier = ''
-        if(mf):
-            modifier = 'AND A.gender= "' + mf + '"'
-
-        sql=('SELECT X.year AS prev_year, Y.year AS  next_year , X.territory, X.x AS prev_rate, Y.x AS next_rate '
-            'FROM '
-            '(SELECT A.year, A.territory, CAST(SUM(A.count) AS REAL)/SUM(B.count) AS x '
-                'FROM matura A, matura B '
-                'WHERE A.type="zdało" '
-                'AND B.type="przystąpiło" '
-                'AND A.territory = B.territory '
-                'AND A.year = B.year '
-                'AND A.gender = B.gender '+modifier+' '
-                'GROUP BY A.year, A.territory) X, '
-            '(SELECT A.year, A.territory, CAST(SUM(A.count) AS REAL)/SUM(B.count) AS x '
-                'FROM matura A, matura B '
-                'WHERE A.type="zdało" '
-                'AND B.type="przystąpiło" '
-                'AND A.territory = B.territory '
-                'AND A.year = B.year '
-                'AND A.gender = B.gender '+modifier+' '
-                'GROUP BY A.year, A.territory) Y '
-            'WHERE X.year + 1 = Y.year '
-            'AND X.territory = Y.territory '
-            'AND X.x > Y.x')
-        result = self.dataHandler.raw_select(sql)
-
-        for row in result:
-            print(row)
-
-    def compare_two_territories(self, territoryA, territoryB, mf=None):
-        modifier = ''
-        if(mf):
-            modifier = 'AND A.gender= "' + mf + '"'
-
-        sql=('SELECT year, territory, MAX(X.x) ' 
-            'FROM '
-            '(SELECT A.year, A.territory, CAST(SUM(A.count) AS REAL)/SUM(B.count) as x '
-                'FROM matura A, matura B '
-                'WHERE A.type="zdało" '
-                'AND B.type="przystąpiło" '
-                'AND A.territory = B.territory '
-                'AND A.year = B.year '
-                'AND A.gender = B.gender '+modifier+' '
-                'AND (A.territory=? OR A.territory=?) ' 
-                'GROUP BY A.year, A.territory) X '
-            'GROUP BY year')
-        result = self.dataHandler.raw_select(sql, (territoryA, territoryB))
-        for row in result:
-            print(row)  
-
-"""
+ 
 def main():
-    m = Matura('tttest.csv')
-    m.calculate_mean('Pomorskie', 2015)
-    print('\n')
-    m.calculate_yearly_pass_rate('Pomorskie')
-    print('\n')
-    m.find_best_territory()
-    print('\n')
-    m.find_regressive_territories()
-    print('\n')
-    m.compare_two_territories('Pomorskie', 'Podlaskie')
+    ui = UserInterface()
+    ui.parse()
 
 main()
-"""
